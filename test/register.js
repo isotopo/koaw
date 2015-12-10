1a6f5955ae8dfb24a98fea1cc3a0ab489b9636ed
Marco Godínez <markotom@gmail.com>
2015-12-10 01:49:28 -0600

    Separate register method from test routes

---
+++ test/register.js
@@ -0,0 +1,52 @@
+'use strict'
+
+const assert = require('assert')
+const RestController = require('../lib')
+const orm = require('./fixtures/orm')
+const sinon = require('sinon')
+
+describe('controller#register', function () {
+  before(function *() {
+    this.orm = yield orm()
+    this.model = this.orm.collections.store
+  })
+
+  it('should fail when trying to register routes', function (done) {
+    let controller = new RestController(this.model)
+    try {
+      controller.register()
+      done('Should have failed when not specify a server')
+    } catch (e) {
+      assert.equal(e.message, 'Controller needs a server to register routes')
+      done()
+    }
+  })
+
+  it('should set a server property as private instance property', function () {
+    let controller = new RestController(this.model)
+    controller.register(this.server)
+    assert(controller._server)
+  })
+
+  it('should ensure to use a middleware server', function (done) {
+    let controller = new RestController(this.model)
+    try {
+      controller.register({ method: function () {} })
+      done('Should have failed when is not a koa server')
+    } catch (e) {
+      assert.equal(e.message, 'Controller needs a middleware server to register routes')
+      done()
+    }
+  })
+
+  it('should set route through server.use method', function *() {
+    let spy = sinon.spy(this.server, 'use')
+    let controller = new RestController(this.model)
+    controller.register(this.server)
+    assert.equal(spy.callCount, 5)
+  })
+
+  after(function () {
+    this.orm.teardown()
+  })
+})
--- test/routes.js
+++ test/routes.js
@@ -6,61 +6,21 @@ const RestController = require('../lib')
 const request = require('supertest')
 const server = require('./fixtures/server')
 const orm = require('./fixtures/orm')
-const sinon = require('sinon')
 
 describe('controller#register', function () {
   before(function *() {
     this.server = yield server()
     this.orm = yield orm()
-    this.model = this.orm.collections.store
     this.agent = request(this.server.listen())
-  })
 
-  it('should fail when trying to register routes', function (done) {
-    let controller = new RestController(this.model)
-    try {
-      controller.register()
-      done('Should have failed when not specify a server')
-    } catch (e) {
-      assert.equal(e.message, 'Controller needs a server to register routes')
-      done()
-    }
-  })
-
-  it('should set a server property as private instance property', function () {
-    let controller = new RestController(this.model)
-    controller.register(this.server)
-    assert(controller._server)
-  })
-
-  it('should ensure to use a middleware server', function (done) {
-    let controller = new RestController(this.model)
-    try {
-      controller.register({ method: function () {} })
-      done('Should have failed when is not a koa server')
-    } catch (e) {
-      assert.equal(e.message, 'Controller needs a middleware server to register routes')
-      done()
-    }
-  })
-
-  before(function () {
-    this.controller = new RestController(this.model)
-    this.controller.methods('post get')
+    this.controller = new RestController(this.orm.collections.store)
+    this.controller.methods('post get put delete')
     this.controller.register(this.server)
 
     this.params = {
       name: faker.lorem.words()[0],
       description: faker.lorem.paragraph()
     }
-  })
-
-  it('should set route through server.use method', function *() {
-    let spy = sinon.spy(this.server, 'use')
-    let controller = new RestController(this.model)
-    controller.register(this.server)
-    // create, collection, single, update, delete
-    assert.equal(spy.callCount, 5)
   })
 
   it('should set route to create a document', function *() {
